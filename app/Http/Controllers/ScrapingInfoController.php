19da9c3fb56351548453ac5e59dd55ec31b3c882
Mindaugas Bernotas <mindaugasbernotas2@gmail.com>
2017-08-28 22:26:18 +0300

    added simple example scraping

--- app/Http/Controllers/ScrapingInfoController.php
+++ app/Http/Controllers/ScrapingInfoController.php
@@ -2,28 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use Gidlov\Copycat\Copycat;
 use Illuminate\Http\Request;
 
 class ScrapingInfoController extends Controller
 {
     public function index()
     {
-        $info = file_get_contents('https://www.norwegian.com/uk/booking/flight-tickets/select-flight/
-        ?D_City=OSL
-        &A_City=RIX
-        &TripType=1
-        &D_Day=01
-        &D_Month=201710
-        &D_SelectedDay=01
-        &R_Day=01
-        &R_Month=201710
-        &R_SelectedDay=01
-        &IncludeTransit=false
-        &AgreementCodeFK=-1
-        &CurrencyCode=GBP
-        &rnd=90232
-        &processid=49841');
-        dd($info);
+        $info = file_get_contents('https://www.norwegian.com/uk/booking/flight-tickets/select-flight/?D_City=OSL&A_City=RIX&TripType=1&D_Day=01&D_Month=201710&D_SelectedDay=01&R_Day=01&R_Month=201710&R_SelectedDay=01&IncludeTransit=false&AgreementCodeFK=-1&CurrencyCode=GBP&rnd=90232&processid=49841');
+
+//        require_once('copycat.php');
+        $cc = new Copycat;
+        $cc->setCURL(array(
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_CONNECTTIMEOUT => 5,
+            CURLOPT_HTTPHEADER, "Content-Type: text/html; charset=iso-8859-1",
+            CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.57 Safari/537.17',
+        ));
+
+        $cc->match(array(
+            'score' => '/itemprop="ratingValue">(.*?)</ms',))->URLs('http://www.imdb.com/title/tt0246578/');
+        $info = $cc->get();
+      dd($info);
+//
         return view('info',$info);
     }
 }
