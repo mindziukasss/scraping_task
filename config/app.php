dbb100ef365e1635f466c37a585d5bd93cb5ef10
Mindaugas Bernotas <mindziukasss@gmail.com>
2017-08-28 15:31:53 +0300

    added gidlov/copycat packagist

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
          * Package Service Providers...
          */
         Laravel\Tinker\TinkerServiceProvider::class,
+        Gidlov\Copycat\CopycatServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -225,6 +226,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Copycat' => Gidlov\Copycat\Copycat::class,
 
     ],
 
