35d82f612e7866a8c60da6334cf8d0242c8c3e3e
Mindaugas Bernotas <mindziukasss@gmail.com>
2017-08-29 10:08:25 +0300

    show cheapest price

--- app/Http/Controllers/ScrapingInfoController.php
+++ app/Http/Controllers/ScrapingInfoController.php
@@ -10,8 +10,8 @@ class ScrapingInfoController extends Controller
     public function index()
     {
         $info = file_get_contents('https://www.norwegian.com/uk/booking/flight-tickets/select-flight/?D_City=OSL&A_City=RIX&TripType=1&D_Day=01&D_Month=201710&D_SelectedDay=01&R_Day=01&R_Month=201710&R_SelectedDay=01&IncludeTransit=false&AgreementCodeFK=-1&CurrencyCode=GBP&rnd=90232&processid=49841');
+//        dd($info);
 
-//        require_once('copycat.php');
         $cc = new Copycat;
         $cc->setCURL(array(
             CURLOPT_RETURNTRANSFER => 1,
@@ -20,11 +20,11 @@ class ScrapingInfoController extends Controller
             CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.57 Safari/537.17',
         ));
 
-        $cc->match(array(
-            'score' => '/itemprop="ratingValue">(.*?)</ms',))->URLs('http://www.imdb.com/title/tt0246578/');
+        $cc->matchAll(array(
+            'price' => '/title="GBP">(.*?)</ms',))->URLs('https://www.norwegian.com/uk/booking/flight-tickets/select-flight/?D_City=OSL&A_City=RIX&TripType=1&D_Day=01&D_Month=201710&D_SelectedDay=01&R_Day=01&R_Month=201710&R_SelectedDay=01&IncludeTransit=false&AgreementCodeFK=-1&CurrencyCode=GBP&rnd=90232&processid=49841');
         $info = $cc->get();
-      dd($info);
-//
+        $info['price'] = $info;
+
         return view('info',$info);
     }
 }
--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -1,1 +1,6 @@
-{{dd($info)}}
+<h3>Norwegian</h3>
+@foreach($price[0] as $key => $value)
+<h2>Best price: {{end($value)}} GBP</h2>
+@endforeach
+
+
